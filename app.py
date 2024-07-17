214157a1179a2a8fa6669d0fb10db9fb37c447b4
Pramit Yeole <pyeole@rare.org>
2024-07-17 15:00:49 -0400

    adding core files for Doc Translator

--- app.py
+++ app.py
@@ -9,13 +9,16 @@ from io import StringIO
 from io import BytesIO
 import os
 from dotenv import load_dotenv
+import traceback
+import logging
 
 load_dotenv()
 auth_key = os.getenv("DEEPL_API_KEY")
 
 translator = deepl.Translator(auth_key=auth_key)
 
-st.header("Translate to your target Language")
+st.image("images\\4729_Brand_USA - HQ_Rare_RGB_Digital_Use.jpg", width=50)
+st.header("The Translator")
 
 selected_language = st.selectbox("Select Target Language:",
                        ("English (American)", "English (British)", "Japanese", "German", "French", "Portugese (Brazilian)", "Portugese (Except Brazilian)"), index=0)
@@ -32,54 +35,71 @@ map = {
 
 target_language = map.get(selected_language)
 
-st.subheader("Enter Text for Translation:")
+text_column, doc_column = st.columns(2)
+open_text_translation = text_column.button('Translate Text', type='primary', key='open text translator')
+open_doc_translation = doc_column.button(label="Translate Document", type='secondary', key='open document translator')
 
-try:
-    text = st.text_area("Enter Text for translation to your target language and hit 'Ctrl + Enter'", value="")
-    text_result = translator.translate_text(text=text, target_lang=target_language)
-    st.write(f"Translating input to {selected_language} ...")
-    st.divider()
-    st.text(text_result)
-    st.divider()
-except Exception as e:
-    st.divider()
-    st.write("Enter Text to translate")
-    st.divider()
-
-st.subheader("Input file for Translation:")
-
-
-# The API also supports .docx, .pptx, .xlsx, .txt, PDF, and HTML files.
-uploaded_files = st.file_uploader("Choose Files to Translate", accept_multiple_files=True)
-for uploaded_file in uploaded_files:
-    with open(uploaded_file.name, "wb") as f:
-        f.write(uploaded_file.getbuffer())
+# Text Translation
+if open_text_translation:
+    st.subheader("Enter Text for Translation:")
 
     try:
-        output_path = f"{selected_language}_{uploaded_file.name}"
-
-        # translation
-        start = time.perf_counter()
-        translator.translate_document_from_filepath(input_path=uploaded_file.name,
-                                    output_path=output_path,
-                                    target_lang=target_language)
-
-        stop = time.perf_counter()
-        # with st.spinner("Translation in progress.."):
-        #     time.sleep(stop - start)
-
-        st.success("Translation successful! Download your translated file below.", icon='✅')
-        with open(output_path, "rb") as file:
-            button = st.download_button(
-                label=f"Download {target_language}_{uploaded_file.name}",
-                data=file,
-                file_name=output_path,
-                mime='application/octet-stream'
-            )
-        
-        if button is not None:
-            os.remove(output_path)
-            os.remove(uploaded_file.name)
-
+        text = st.text_area("Enter Text for translation to your target language and hit 'Ctrl + Enter'", value="")
+        text_result = translator.translate_text(text=text, target_lang=target_language)
+        st.write(f"Translating input to {selected_language} ...")
+        st.divider()
+        st.text(text_result)
+        st.divider()
     except Exception as e:
-        st.error(f'We encountered an error: {e}', icon="🚨")
+        st.divider()
+        st.write("Enter Text to translate")
+        st.divider()
+        traceback.print_exc()
+
+# Document Translation
+else:
+    st.subheader("Translate Documents")
+    st.write("Ensure that your target language is different from the language of your document!")
+    # The API also supports .docx, .pptx, .xlsx, .txt, PDF, and HTML files.
+    uploaded_files = st.file_uploader("Choose Files to Translate", accept_multiple_files=True)
+    for uploaded_file in uploaded_files:
+        doc_translate = st.button(f"Translate {uploaded_file.name}", key=f"{uploaded_file}_Begin Translation")
+        if doc_translate:
+            with open(uploaded_file.name, "wb") as f:
+                f.write(uploaded_file.getbuffer())
+
+            try:
+                output_path = f"files\\{selected_language}_{uploaded_file.name}"
+
+                # translation
+                translation_status = st.empty()
+                translation_status.text("Translation in Progress....")
+
+                translator.translate_document_from_filepath(input_path=uploaded_file.name,
+                                            output_path=output_path,
+                                            target_lang=target_language)
+                translation_status.empty()
+                st.success("Translation successful! Download your translated file below...", icon='✅')
+                time.sleep(2)
+                translation_status.empty()
+                with open(output_path, "rb") as file:
+                    button = st.download_button(
+                        label=f"Download {target_language}_{uploaded_file.name}",
+                        data=file,
+                        file_name=output_path,
+                        mime='application/octet-stream'
+                    )
+
+                if button:
+                    os.remove(output_path)
+            except Exception as e:
+                st.error(f'The translation could not be completed. We encountered an error: \n{e}', icon="🚨")
+                traceback.print_exc()
+            finally:
+                os.remove(uploaded_file.name)
+
+
+st.divider()
+
+disclaimer = st.empty()
+disclaimer = st.text("For internal use only!")
